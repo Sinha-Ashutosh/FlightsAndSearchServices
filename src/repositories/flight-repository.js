da08b1d47d3c7d9698e3fc76877f5defb0c101dd
Ashutosh Sinha <Ashutosh-Sinha@outlook.com>
2024-08-19 20:43:55 +0530

    Added Flights search crud with few filters

--- src/repositories/flight-repository.js
+++ src/repositories/flight-repository.js
@@ -1,6 +1,32 @@
 const {Flights} = require('../models/index');
+const { Op } = require('sequelize');
 
 class FlightRepository {
+
+    #createFilter(data) {
+        let filter = {};
+        if(data.arrivalAirportId) {
+            filter.arrivalAirportId = data.arrivalAirportId;
+        }
+        if(data.departureAirportId) {
+            filter.departureAirportId = data.departureAirportId;
+        }
+        if (data.minPrice || data.maxPrice) {
+            filter.price = {};
+            if (data.minPrice) {
+                filter.price[Op.gte] = data.minPrice;
+            }
+            if (data.maxPrice) {
+                filter.price[Op.lt] = data.maxPrice;
+            }
+        /*
+        if(data.minPrice) {
+            Object.assign(filter, {price: {[Op.gte]: data.minPrice}});       
+        }
+         */
+        return filter;
+    }
+}
 
     async createFlight(data) {
         try {
@@ -11,6 +37,40 @@ class FlightRepository {
             throw {error};
         }
     }
+
+    async getFlight(flightId){
+        try {
+            const flight = await Flights.findByPk(flightId);
+            return flight;
+        }catch (error) {
+            console.log("Somethinng went wrong in repository level");
+            throw {error};
+        }
+    }
+
+    async getAllFlights(filter) {
+        try {
+            const filterObject = this.#createFilter(filter);
+            const flight = await Flights.findAll({
+                where: filterObject
+            });
+            return flight;
+        }catch (error) {
+            console.log("Somethinng went wrong in repository level");
+            throw {error};
+        }
+    }
+
+
 }
 
 module.exports = FlightRepository;
+/*
+ {
+    where: {
+        arrivalAirportId: 2,
+        departureAirportId: 4,
+        price: {[Op.gte]: 4000}
+    }
+ }
+*/
